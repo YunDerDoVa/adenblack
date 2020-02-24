e75d32b1a67c11d32f45db02427bd70e8fde15e8
ArthurNeyer <arthur.neyer@protonmail.com>
2020-02-24 20:25:22 +0100

    Dev mode

--- adenblack/settings.py
+++ adenblack/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@cjt(uj16%vit@&c4b!s&4hhhmk3e)j--#o6e#*^$-%!z*=qmg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'adenblack.herokuapp.com',
